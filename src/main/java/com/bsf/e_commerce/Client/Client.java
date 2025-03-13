10b52cea3535df3cfa182ea02764add9e7b5ed68
Brenno-SF <brprogrammersf@gmail.com>
2025-03-13 01:23:40 -0300

    feat: adds client address insertion

--- src/main/java/com/bsf/e_commerce/Client/Client.java
+++ src/main/java/com/bsf/e_commerce/Client/Client.java
@@ -1,6 +1,6 @@
 package com.bsf.e_commerce.Client;
 
-import com.bsf.e_commerce.product.ProductRequestDTO;
+import com.bsf.e_commerce.Address.Address;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -24,7 +24,10 @@ public class Client {
     private String password;
     private String email;
     private LocalDate birth_date;
-    //private Adress adress;
+
+    @OneToOne(mappedBy = "client", cascade = CascadeType.ALL)
+    private Address address;
+
 
     public Client(ClientRequestDTO data) {
        this.name_client = data.name_client();
@@ -34,6 +37,11 @@ public class Client {
        this.email = data.email();
        this.birth_date = data.birth_date();
        this.balance = data.balance();
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+        address.setClient(this);
     }
 }
 
--- src/main/java/com/bsf/e_commerce/Client/ClientResponseDTO.java
+++ src/main/java/com/bsf/e_commerce/Client/ClientResponseDTO.java
@@ -1,10 +1,12 @@
 package com.bsf.e_commerce.Client;
 
+import com.bsf.e_commerce.Address.Address;
+
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
-public record ClientResponseDTO( String id_client,String name_client, String cpf, String username, String password, String email, LocalDate birth_date, BigDecimal balance) {
+public record ClientResponseDTO(String id_client, String name_client, String cpf, String username, String password, String email, LocalDate birth_date, BigDecimal balance, Address address) {
     public ClientResponseDTO(Client client) {
-        this(client.getId_client(), client.getName_client(), client.getCpf(), client.getUsername(), client.getPassword(), client.getEmail(), client.getBirth_date(), client.getBalance());
+        this(client.getId_client(), client.getName_client(), client.getCpf(), client.getUsername(), client.getPassword(), client.getEmail(), client.getBirth_date(), client.getBalance(), client.getAddress());
     }
 }
