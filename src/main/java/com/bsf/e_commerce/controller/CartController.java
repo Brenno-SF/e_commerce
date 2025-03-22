2cc513f8c0d6ac6f9781c738f5a1b69e4dfac7c1
Brenno-SF <brprogrammersf@gmail.com>
2025-03-22 20:31:57 -0300

    feat: adds Post method to CartController

--- src/main/java/com/bsf/e_commerce/controller/CartController.java
+++ src/main/java/com/bsf/e_commerce/controller/CartController.java
@@ -1,11 +1,19 @@
 package com.bsf.e_commerce.controller;
 
+import com.bsf.e_commerce.entity.Cart;
+import com.bsf.e_commerce.entity.Client;
+import com.bsf.e_commerce.entity.Product;
 import com.bsf.e_commerce.repository.CartRepository;
+import com.bsf.e_commerce.repository.ClientRepository;
+import com.bsf.e_commerce.repository.ProductRepository;
+import com.bsf.e_commerce.request.CartRequestDTO;
 import com.bsf.e_commerce.response.CartResponseDTO;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -14,6 +22,10 @@ import java.util.stream.Collectors;
 public class CartController {
     @Autowired
     private CartRepository cartRepository;
+    @Autowired
+    private ClientRepository clientRepository;
+    @Autowired
+    private ProductRepository productRepository;
 
     @CrossOrigin(origins = "*", allowedHeaders = "*")
     @GetMapping
@@ -27,5 +39,23 @@ public class CartController {
     public ResponseEntity<List<CartResponseDTO>> getByIdClient(@PathVariable String fk_client){
         List<CartResponseDTO> cart = cartRepository.findByClientId(fk_client);
         return ResponseEntity.ok(cart);
+    }
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping
+    public ResponseEntity<String> saveCart(@RequestBody CartRequestDTO data){
+        Client client = clientRepository.findById(data.client().getIdClient())
+                .orElseThrow(() -> new RuntimeException("Client not found"));
+        Product product = productRepository.findById(data.product().getId_product())
+                .orElseThrow(() -> new RuntimeException("Product not found"));
+
+        Cart cart = new Cart();
+        cart.setClient(client);
+        cart.setProduct(product);
+        cart.setQuantity(data.quantity());
+        cart.setAdded_at(LocalDateTime.now());
+        cartRepository.save(cart);
+
+        return ResponseEntity.status(HttpStatus.CREATED).body(cart + " saved successfully");
+
     }
 }
